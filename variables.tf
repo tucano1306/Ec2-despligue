e7d8555e2138c76ebee7c31eed59b8d0083c9a7d
Leonardo <66296967+tucano1306@users.noreply.github.com>
2024-08-15 20:06:10 -0400

    Update variables.tf
    cambiando region y ami

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources"
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
-  default     = "ami-0a38c1c38a15fed74"
+  default     = "ami-0ae8f15ae66fe8cda"
 }
 
 variable "instance_type" {
